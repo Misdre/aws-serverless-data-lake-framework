8c2c4299494c6531fff710a52fabbbb3c242eb0e
Misdre <aws@misdre.info>
2022-09-23 15:13:05 +0000

    black

--- sdlf-foundations/lambda/routing/src/lambda_function.py
+++ sdlf-foundations/lambda/routing/src/lambda_function.py
@@ -14,20 +14,20 @@ session_config = Config(user_agent_extra="awssdlf/1.0.0")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-events = boto3.client('events', config=session_config)
+events = boto3.client("events", config=session_config)
 dynamodb = boto3.resource("dynamodb", config=session_config)
-dataset_table = dynamodb.Table('octagon-Datasets-{}'.format(os.environ['ENV']))
-pipeline_table = dynamodb.Table('octagon-Pipelines-{}'.format(os.environ['ENV']))
+dataset_table = dynamodb.Table("octagon-Datasets-{}".format(os.environ["ENV"]))
+pipeline_table = dynamodb.Table("octagon-Pipelines-{}".format(os.environ["ENV"]))
 
 
 def parse_s3_event(s3_event):
     return {
-        'bucket': s3_event['s3']['bucket']['name'],
-        'key': unquote_plus(s3_event['s3']['object']['key']),
-        'size': s3_event['s3']['object']['size'],
-        'last_modified_date': s3_event['eventTime'].split('.')[0]+'+00:00',
-        'timestamp': int(round(datetime.utcnow().timestamp()*1000, 0)),
-        'stage': 'raw'
+        "bucket": s3_event["s3"]["bucket"]["name"],
+        "key": unquote_plus(s3_event["s3"]["object"]["key"]),
+        "size": s3_event["s3"]["object"]["size"],
+        "last_modified_date": s3_event["eventTime"].split(".")[0] + "+00:00",
+        "timestamp": int(round(datetime.utcnow().timestamp() * 1000, 0)),
+        "stage": "raw",
     }
 
 
@@ -39,11 +39,11 @@ def paginate_dynamodb_response(dynamodb_action, **kwargs):
 
     while not done:
         if start_key:
-            keywords['ExclusiveStartKey'] = start_key
+            keywords["ExclusiveStartKey"] = start_key
 
         response = dynamodb_action(**keywords)
 
-        start_key = response.get('LastEvaluatedKey', None)
+        start_key = response.get("LastEvaluatedKey", None)
         done = start_key is None
 
         for item in response.get("Items", []):
@@ -52,69 +52,60 @@ def paginate_dynamodb_response(dynamodb_action, **kwargs):
 
 def get_item(table, team, dataset):
     try:
-        response = table.get_item(
-            Key={
-                'name': '{}-{}'.format(team, dataset)
-            }
-        )
+        response = table.get_item(Key={"name": "{}-{}".format(team, dataset)})
     except ClientError as e:
-        print(e.response['Error']['Message'])
+        print(e.response["Error"]["Message"])
     else:
-        item = response['Item']
-        return item['pipeline']
+        item = response["Item"]
+        return item["pipeline"]
 
 
 def get_pipeline_entrypoint(table, team, pipeline):
     query = paginate_dynamodb_response(
         table.query,
-        IndexName='entrypoint-name-index',
-        KeyConditionExpression=Key('entrypoint').eq('true') & Key('name').begins_with('{}-{}-'.format(team, pipeline))
+        IndexName="entrypoint-name-index",
+        KeyConditionExpression=Key("entrypoint").eq("true") & Key("name").begins_with("{}-{}-".format(team, pipeline)),
     )
 
     for row in query:
-        return row['name'].removeprefix('{}-{}-'.format(team, pipeline))
+        return row["name"].removeprefix("{}-{}-".format(team, pipeline))
     else:
-        raise Exception('The selected pipeline does not have an entry point.')
+        raise Exception("The selected pipeline does not have an entry point.")
 
 
 def lambda_handler(event, context):
     try:
-        logger.info('Received {} messages'.format(len(event['Records'])))
-        for record in event['Records']:
-            logger.info('Parsing S3 Event')
-            message = parse_s3_event(json.loads(record['body']))
+        logger.info("Received {} messages".format(len(event["Records"])))
+        for record in event["Records"]:
+            logger.info("Parsing S3 Event")
+            message = parse_s3_event(json.loads(record["body"]))
 
-            if os.environ['NUM_BUCKETS'] == '1':
-                team = message['key'].split('/')[1]
-                dataset = message['key'].split('/')[2]
+            if os.environ["NUM_BUCKETS"] == "1":
+                team = message["key"].split("/")[1]
+                dataset = message["key"].split("/")[2]
             else:
-                team = message['key'].split('/')[0]
-                dataset = message['key'].split('/')[1]
-            message['team'] = team
-            message['dataset'] = dataset
+                team = message["key"].split("/")[0]
+                dataset = message["key"].split("/")[1]
+            message["team"] = team
+            message["dataset"] = dataset
             pipeline = get_item(dataset_table, team, dataset)
-            message['pipeline'] = pipeline
-            message['org'] = os.environ['ORG']
-            message['app'] = os.environ['APP']
-            message['env'] = os.environ['ENV']
+            message["pipeline"] = pipeline
+            message["org"] = os.environ["ORG"]
+            message["app"] = os.environ["APP"]
+            message["env"] = os.environ["ENV"]
             pipeline_stage = get_pipeline_entrypoint(pipeline_table, team, pipeline)
-            logger.info('Pipeline entrypoint: {}'.format(pipeline_stage))
-            message['pipeline_stage'] = pipeline_stage
+            logger.info("Pipeline entrypoint: {}".format(pipeline_stage))
+            message["pipeline_stage"] = pipeline_stage
 
-            logger.info(
-                'Sending events to default event bus for processing')
+            logger.info("Sending events to default event bus for processing")
             entries = events.put_events(
                 Entries=[
-                    {
-                        'Source': 'sdlf.s3',
-                        'DetailType': 'SdlfObjectInS3',
-                        'Detail': json.dumps(message)
-                    },
+                    {"Source": "sdlf.s3", "DetailType": "SdlfObjectInS3", "Detail": json.dumps(message)},
                 ]
             )
 
-            if entries['FailedEntryCount'] > 0:
-                raise Exception('Some entries could not be sent to EventBridge: {}'.format(entries))
+            if entries["FailedEntryCount"] > 0:
+                raise Exception("Some entries could not be sent to EventBridge: {}".format(entries))
     except Exception as e:
         logger.error("Fatal error", exc_info=True)
         raise e
--- sdlf-stageB/lambda/stage-b-routing/src/lambda_function.py
+++ sdlf-stageB/lambda/stage-b-routing/src/lambda_function.py
@@ -22,57 +22,54 @@ def fetch_messages(team, pipeline, stage):
     sqs_config = SQSConfiguration(team, pipeline, stage)
     queue_interface = SQSInterface(sqs_config.get_stage_queue_name)
     min_items_to_process = 1
-    max_items_to_process = 100 # TODO implement this at the pipeline level
-    logger.info(
-        'Querying {}-{}-{} objects waiting for processing'.format(team, pipeline, stage))
-    keys_to_process = queue_interface.receive_min_max_messages(
-        min_items_to_process, max_items_to_process)
+    max_items_to_process = 100  # TODO implement this at the pipeline level
+    logger.info("Querying {}-{}-{} objects waiting for processing".format(team, pipeline, stage))
+    keys_to_process = queue_interface.receive_min_max_messages(min_items_to_process, max_items_to_process)
 
-    logger.info('{} Objects ready for processing'.format(
-        len(keys_to_process)))
+    logger.info("{} Objects ready for processing".format(len(keys_to_process)))
     keys_to_process = list(set(keys_to_process))
 
     return keys_to_process
+
 
 def lambda_handler(event, context):
     """Checks if any items need processing and triggers state machine
     Arguments:
         event {dict} -- Dictionary with details on what needs processing
-        context {dict} -- Dictionary with details on Lambda context 
+        context {dict} -- Dictionary with details on Lambda context
     """
 
     # TODO Implement Redrive Logic (through message_group_id)
     try:
         keys_to_process = []
-        trigger_type = event.get('trigger_type') # this is set by the schedule event rule
+        trigger_type = event.get("trigger_type")  # this is set by the schedule event rule
         if trigger_type:
-            records = fetch_messages(event['team'], event['pipeline'], event['pipeline_stage'])
+            records = fetch_messages(event["team"], event["pipeline"], event["pipeline_stage"])
         else:
-            records = event['Records']
-        logger.info('Received {} messages'.format(len(records)))
+            records = event["Records"]
+        logger.info("Received {} messages".format(len(records)))
         for record in records:
             if trigger_type:
-                event_body = json.loads(json.loads(record)['output'])[0]['body']
+                event_body = json.loads(json.loads(record)["output"])[0]["body"]
             else:
-                event_body = json.loads(json.loads(record['body'])['output'])[0]['body']
+                event_body = json.loads(json.loads(record["body"])["output"])[0]["body"]
 
-            team = event_body['team']
-            pipeline = event_body['pipeline']
-            stage = os.environ['PIPELINE_STAGE']
-            dataset = event_body['dataset']
-            org = event_body['org']
-            app = event_body['app']
-            env = event_body['env']
+            team = event_body["team"]
+            pipeline = event_body["pipeline"]
+            stage = os.environ["PIPELINE_STAGE"]
+            dataset = event_body["dataset"]
+            org = event_body["org"]
+            app = event_body["app"]
+            env = event_body["env"]
             stage_bucket = S3Configuration().stage_bucket
-            keys_to_process = event_body['processedKeys']
+            keys_to_process = event_body["processedKeys"]
 
-            logger.info('{} Objects ready for processing'.format(
-                len(keys_to_process)))
+            logger.info("{} Objects ready for processing".format(len(keys_to_process)))
             keys_to_process = list(set(keys_to_process))
 
             response = {
-                'statusCode': 200,
-                'body': {
+                "statusCode": 200,
+                "body": {
                     "bucket": stage_bucket,
                     "keysToProcess": keys_to_process,
                     "team": team,
@@ -81,20 +78,18 @@ def lambda_handler(event, context):
                     "dataset": dataset,
                     "org": org,
                     "app": app,
-                    "env": env
-                }
+                    "env": env,
+                },
             }
-            logger.info('Starting State Machine Execution')
+            logger.info("Starting State Machine Execution")
             state_config = StateMachineConfiguration(team, pipeline, stage)
-            StatesInterface().run_state_machine(
-                state_config.get_stage_state_machine_arn, response)
+            StatesInterface().run_state_machine(state_config.get_stage_state_machine_arn, response)
     except Exception as e:
         # If failure send to DLQ
         if keys_to_process:
             sqs_config = SQSConfiguration(team, pipeline, stage)
             dlq_interface = SQSInterface(sqs_config.get_stage_dlq_name)
-            dlq_interface.send_message_to_fifo_queue(
-                json.dumps(response), 'failed')
+            dlq_interface.send_message_to_fifo_queue(json.dumps(response), "failed")
         logger.error("Fatal error", exc_info=True)
         raise e
     return
